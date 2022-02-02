bf338a882e27b3bba4b30e4c376c523c15d952b4
Alokam Chinenye Augusta <61966991+Augusta02@users.noreply.github.com>
2022-02-02 12:39:43 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,42 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+print('Chinenye')
+print('Alokam')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+#Check the data types of the following
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Alokam'))
+print(type('Augusta'))
+print(type("Nigeria"))
+
+#Exercise 3
+#Write an example for different Python data types such as Number (Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print(type(10))
+print(type(20.9))
+print(type(4 - 5j))
+print(type("school is great"))
+print(type(4 > 10))
+print(type(['Data Science', 'Data Engineer', 'Data Analyst']))
+print(type((30,40,50)))
+print(type({30, 40 ,50}))
+print(type({'numbers': [30,40,50]}))
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+p1, p2 = (2, 3)
+q1, q2 = (10, 8)
+y = (q1 - p1) ** 2
+print(y)
+x = (q2 - p1) ** 2
+print(x)
+z = (y + x) ** 0.5
+print("Euclidian distance:", z)
